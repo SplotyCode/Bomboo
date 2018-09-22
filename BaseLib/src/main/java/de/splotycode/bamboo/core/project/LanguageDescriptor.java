01a9e4056647fa1463c35835e67078f09c51ed3e
david <davidscandurra@gmail.com>
2018-09-22 23:49:46 +0200

    Updated LanguageDescriptor & Delete Java Editor

--- BaseLib/src/main/java/de/splotycode/bamboo/core/project/LanguageDescriptor.java
+++ BaseLib/src/main/java/de/splotycode/bamboo/core/project/LanguageDescriptor.java
@@ -1,10 +1,17 @@
 package de.splotycode.bamboo.core.project;
 
+import de.splotycode.bamboo.core.editor.Editor;
 import de.splotycode.bamboo.core.yaml.Configuration;
+
+import java.io.File;
 
 public interface LanguageDescriptor {
 
-    void load(Project project, Configuration configuration);
+    void load(Project project, Configuration configuration, WorkSpace workSpace);
+
+    String loadContent(File file);
+
+    void prepairEditor(Editor editor, WorkSpace workSpace);
 
     String[] fileTypes();
 
